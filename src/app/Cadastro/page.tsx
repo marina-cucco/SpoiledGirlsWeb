67dc3e350659643c5c8365669bf99268a0c0a841
marina-cucco <marinascucco@hotmail.com>
2024-04-16 01:10:38 -0300

    feat: começo da pagina de cadastro

--- src/app/Cadastro/page.tsx
+++ src/app/Cadastro/page.tsx
@@ -5,7 +5,28 @@ export default function Cadastro() {
     <main className="flex min-h-screen flex-col items-center">
       <NavBar active="cadastro"/>
 
-      <h2>Categorias</h2>
+      <h3 className='ml-20 text-slate-950' 
+        style={{ fontSize: "1.80rem", fontFamily: "Prata, serif" }}>
+          Seja Bem Vinda!
+      </h3>
+      <h1
+        className='ml-20 text-slate-950' 
+        style={{ fontSize: "2.81rem", fontFamily: "Prata, serif" }}
+      >
+        Spoiled Girls Club
+        
+      </h1>
+      <div className="flex-col items-center justify-center my-4">
+      <img src='/assets/line.svg' alt='line'/>
+      <div className="flex space-x-6 items-center justify-center my-4">
+      <img src='/assets/Group 1.svg' alt='google'/>
+      <img src='/assets/Group 2.svg' alt='facebook'/>
+      </div>
+      </div>
+      <h4 className='ml-20 text-slate-950' 
+        style={{ fontSize: "2.81rem", fontFamily: "Prata, serif" }}>
+        Cadastre-se
+        </h4>
     </main>
   );
 }
