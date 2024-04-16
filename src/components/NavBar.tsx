b7cee3b22ae3edcd5f620b7ccfde16f49ca4d65b
thallencar <alencar.thalita@gmail.com>
2024-04-15 23:32:02 -0300

    feat: atualizando a nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,45 @@
 import Link from "next/link";
-
-interface NavBarProps{
-    active: "home" | "cadastro" | "carrinho"
+interface NavBarProps {
+  active: "home" | "cadastro" | "carrinho";
 }
 
-export function NavBar(props: NavBarProps){
-    const active = props.active 
-    const classActive = "border-b-4 border-white pb-3"
+export function NavBar(props: NavBarProps) {
+  const active = props.active;
+  const classActive = "border-b-4 border-white pb-3";
 
-    return (
-    <nav className=" flex justify-between items-center px-8 py-2 bg-zinc-800 w-full">
-        <h1 className="text-4xl font-bold">Spoiled Girls Club</h1>
+  return (
+    <nav className=' flex justify-between items-center px-8 py-2 bg-zinc-900 w-full'>
+      <h1
+        className='ml-20' 
+        style={{ fontSize: "2.81rem", fontFamily: "Prata, serif" }}
+      >
+        Spoiled Girls Club
+      </h1>
+      {/*
         <ul className="flex gap-6">
           <li className={active == "home" ? classActive : ""}>
-            <Link href="/">home</Link>
+            <Link href="/">Home</Link>
           </li>
           <li className={active == "cadastro" ? classActive : ""}>
-            <Link href="/">cadastro</Link>  
+            <Link href="/">Cadastro</Link>  
           </li>
           <li className={active == "carrinho" ? classActive : ""}>
-            <Link href="/">carrinho</Link>
+            <Link href="/">Carrinho</Link>
           </li>
         </ul>
-        <div className="size-14 rounded-full overflow-hidden">
-          <img src="https://i.pravatar.cc/300" alt="avatar do usuario"/>
-        </div>
+        */}
+      <div className='ml-96 pl-48'>
+        <Link href={"/Carrinho/page.tsx"}>
+          <img src='/assets/carrinho.svg' alt='Carrinho de compras'  width="42" height="42"/>
+        </Link>
+      </div>
+      <div className='size-14 rounded-full overflow-hidden mr-24 mb-2'>
+        <img
+          className='flex justify-center'
+          src='/assets/user.svg'
+          alt='Avatar do usuário'
+        />
+      </div>
     </nav>
-    )
+  );
 }
