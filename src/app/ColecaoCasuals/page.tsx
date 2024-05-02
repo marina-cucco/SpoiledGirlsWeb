2ca4fc18ffc02025853202de9d7f3af582e08046
thallencar <alencar.thalita@gmail.com>
2024-05-02 00:40:34 -0300

    feat: criando components das roupas e modificando coleção casuals

--- src/app/ColecaoCasuals/page.tsx
+++ src/app/ColecaoCasuals/page.tsx
@@ -1,20 +1,44 @@
 import { NavBar } from "@/components/NavBar";
 import { NavHome } from "@/components/NavHome";
 import { Footer } from "@/components/Footer";
+import { ContainerRoupa } from "@/components/ContainerRoupa";
 
-export default function ColecaoCasuals(){
-    return(
-        <div style={{ maxHeight: '100vh', overflowY: 'scroll' }}>
-            <main className="flex min-h-screen flex-col items-center">
-                <NavBar/>
-                <NavHome active="colecaoCasuals"/>
-                <h1 className='p-6 mb-4'
-                style={{ fontSize: "2.18rem", fontFamily: "Libre Baskerville, serif", color: "var(--cor-primaria)"}}> 
-                    Essenciais para o seu dia a dia.
-                </h1>
-            </main>
-            <Footer></Footer>
-        </div>
-
-    );
+export default function ColecaoCasuals() {
+  return (
+    <div style={{ maxHeight: "100vh", overflowY: "scroll" }}>
+      <main className='flex min-h-screen flex-col items-center'>
+        <NavBar />
+        <NavHome active='colecaoCasuals' />
+        <h1
+          className='my-6'
+          style={{
+            fontSize: "2.18rem",
+            fontFamily: "Libre Baskerville, serif",
+            color: "var(--cor-primaria)",
+          }}
+        >
+          Essenciais para o seu dia a dia.
+        </h1>
+        <img className='' src='assets/PhotoModel4.png' />
+        <div className="flex items-center justify-center gap-20 mb-16">
+          <ContainerRoupa
+            roupa='Macacão Jeans'
+            preco='249.99'
+            imagem='/assets/Outfits/Casuals1.png'
+          />
+          <ContainerRoupa
+            roupa='T-Shirt Preta'
+            preco='39.99'
+            imagem='/assets/Outfits/Casuals2.png'
+          />
+          <ContainerRoupa
+            roupa='Jaqueta de Couro'
+            preco='119.99'
+            imagem='/assets/Outfits/Casuals3.png'
+          />
+        </div>     
+      </main>
+      <Footer></Footer>
+    </div>
+  );
 }
---
+++ src/components/ContainerRoupa.tsx
@@ -0,0 +1,31 @@
+import { Botao } from "@/components/Botao";
+import Link from "next/link";
+
+export function ContainerRoupa({
+    roupa = "ROUPA", preco = "00,00", imagem = "/assets/", alt = ""}) { 
+  return (
+    
+    <div className="py-10">
+        <img
+         className="w-80 h-80" src={imagem} alt={alt}
+         style={{ 
+            width: "21.25rem",
+            height : "21.25rem",
+        }}/>               
+        <div>
+            <div className="mt-7 mb-4">
+                <h1
+                style={{ fontSize: "2rem", fontFamily: "Prata, serif", color:"var(--cor-primaria)"}}>
+                    {roupa}
+                </h1>
+                <h5 className="font-bold"
+                style={{ fontSize: "1.5rem", fontFamily: "Libre Baskerville, serif", color:"var(--cor-primaria)"}}>
+                    R${preco}
+                </h5>
+            </div>     
+            <Botao texto="COMPRAR" width="21.25rem" height="4rem"></Botao>
+        </div>
+    </div>
+    
+  );
+}
